4a100f4bdf225ea45166da93ff96897d2bbb4d77
Honkou <filiphancke@gmail.com>
2024-11-10 14:20:20 +0100

    Refactor base attributes to be explicitly stated

--- src/characters.py
+++ src/characters.py
@@ -15,19 +15,34 @@ class KnaveAttributes:
     wisdom: int = 0
     charisma: int = 0
 
+    @property
+    def base_attributes(self) -> dict:
+        """Return the base attributes as a dictionary."""
+        return {
+            "strength": self.strength,
+            "dexterity": self.dexterity,
+            "constitution": self.constitution,
+            "intelligence": self.intelligence,
+            "wisdom": self.wisdom,
+            "charisma": self.charisma,
+        }
+
+    def _increase_attribute(self, attribute: str) -> None:
+        """Increase the value of the attribute by one."""
+        setattr(self, attribute, getattr(self, attribute) + 1)
+
     def add_first_level_attributes(self) -> None:
         """Randomly choose one attribute and increase it by one. Do it 3 times."""
-        attributes = list(self.__dict__)
         for _ in range(3):
-            random_stat = random.choice(attributes)
-            setattr(self, random_stat, getattr(self, random_stat) + 1)
+            random_stat = random.choice(list(self.base_attributes.keys()))
+            self._increase_attribute(random_stat)
 
     @property
     def statistics_sum(self) -> int:
         """Return the sum of all 6 statistics. Mostly used as a sanity check."""
         total = 0
-        for stat in self.__dict__:
-            total += getattr(self, stat)
+        for stat in self.base_attributes.values():
+            total += stat
         return total
 
 
--- tests/test_characters.py
+++ tests/test_characters.py
@@ -1,17 +1,17 @@
 """Tests suite for characters."""
 
-from dataclasses import asdict
-
 from characters import KnaveAttributes, KnaveCharacter
 
 
 def test_default_character_creation():
+    """Assert that a default new character has all attributes set to 0."""
     character = KnaveCharacter()
-    for _, value in asdict(character.attributes).items():
+    for value in character.attributes.base_attributes.values():
         assert value == 0
 
 
 def test_1st_level_statistics_creation():
+    """Assert that a 1st level character has a total of 3 points in stats."""
     attributes = KnaveAttributes()
     attributes.add_first_level_attributes()
     assert attributes.statistics_sum == 3
